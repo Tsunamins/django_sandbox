24512fc3edc8b2e861498536fc2bebebaa11780b
Alison Reilly <reillyamr@gmail.com>
2020-09-20 12:51:17 -0400

    add/migrate puppy model for test example

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -3,12 +3,34 @@ from django.db import models
 from django.conf import settings
 
 
-class Ingredient(models.Model):  # Ingredient to be used in a recipe
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-     )
+# class Ingredient(models.Model):  # Ingredient to be used in a recipe
+#     name = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#      )
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
+
+
+# class Recipe(models.Model):
+#     name = models.CharField(max_length=25)
+
+class Puppy(models.Model):
+    """
+    Puppy Model
+    Defines the attributes of a puppy
+    """
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_breed(self):
+        return self.name + ' belongs to ' + self.breed + ' breed.'
+
+    def __repr__(self):
+        return self.name + ' is added.'
