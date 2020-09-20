0f9962818a7ba8d8e39cdb7f059ad91902b693ba
Alison Reilly <reillyamr@gmail.com>
2020-09-20 12:52:36 -0400

    add puppy test example, comment out ingred example

--- thesandbox/sandbox_api/tests.py
+++ thesandbox/sandbox_api/tests.py
@@ -6,35 +6,55 @@ from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APIClient
-from . models import Ingredient, Recipe
-from . serializers import IngredientSerializer
-INGREDIENTS_URL = reverse('recipe:ingredient-list')
+# from . models import Ingredient, Recipe
+# from . serializers import IngredientSerializer
+# INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+from . models import Puppy
 
 
-class PrivateIngredientsApiTest(TestCase):  # Test the private ingredients api
- 
+# class PrivateIngredientsApiTest(TestCase):  # Test the private ingredients api
+
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = get_user_model().objects.create_user(
+#             'ksarthak4ever@gmail',
+#             'hakunamatata'
+#         )
+#         self.client.force_authenticate(self.user)
+
+#     def test_retrieve_ingredients_list(self):  # Test retrieving a list of ingredients
+#         Ingredient.objects.create(user=self.user, name='Potato')
+#         Ingredient.objects.create(user=self.user, name='Peas')
+#         res = self.client.get(INGREDIENTS_URL)
+#         ingredients = Ingredient.objects.all().order_by('-name')
+#         serializer = IngredientSerializer(ingredients, many=True)
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+#         self.assertEqual(res.data, serializer.data)
+
+#     def test_create_ingredient_successful(self):  # Testing create a new ingredient
+#         payload = { 'name': 'Cabbage' }
+#         self.client.post(INGREDIENTS_URL, payload)
+#         exists = Ingredient.objects.filter(
+#             user = self.user,
+#             name = payload['name'],
+#             ).exists()  # chechking if ingredient exists. exists() function will return boolean true or false value
+#         self.assertTrue(exists)
+
+
+class PuppyTest(TestCase):
+    """ Test module for Puppy model """
+
     def setUp(self):
-        self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            'ksarthak4ever@gmail',
-            'hakunamatata'
-        )
-        self.client.force_authenticate(self.user)
+        Puppy.objects.create(
+            name='Casper', age=3, breed='Bull Dog', color='Black')
+        Puppy.objects.create(
+            name='Muffin', age=1, breed='Gradane', color='Brown')
 
-    def test_retrieve_ingredients_list(self):  # Test retrieving a list of ingredients
-        Ingredient.objects.create(user=self.user, name='Potato')
-        Ingredient.objects.create(user=self.user, name='Peas')
-        res = self.client.get(INGREDIENTS_URL)
-        ingredients = Ingredient.objects.all().order_by('-name')
-        serializer = IngredientSerializer(ingredients, many=True)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
-
-    def test_create_ingredient_successful(self):  # Testing create a new ingredient
-        payload = { 'name': 'Cabbage' }
-        self.client.post(INGREDIENTS_URL, payload)
-        exists = Ingredient.objects.filter(
-            user = self.user,
-            name = payload['name'],
-            ).exists()  # chechking if ingredient exists. exists() function will return boolean true or false value
-        self.assertTrue(exists)
+    def test_puppy_breed(self):
+        puppy_casper = Puppy.objects.get(name='Casper')
+        puppy_muffin = Puppy.objects.get(name='Muffin')
+        self.assertEqual(
+            puppy_casper.get_breed(), "Casper belongs to Bull Dog breed.")
+        self.assertEqual(
+            puppy_muffin.get_breed(), "Muffin belongs to Gradane breed.")
