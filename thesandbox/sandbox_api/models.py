9dc9d099c8ea90e51dce721c5a5df7d88c57d7c4
Alison Reilly <reillyamr@gmail.com>
2020-09-26 13:09:50 -0400

    add custom user model

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -2,6 +2,57 @@ from django.db import models
 
 from django.conf import settings
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have an email')
+
+        user = self.model(username=username, email=self.normalize_email(email))    
+        user.set_password(password)
+        user.save()
+
+    def create_superuser(self, username, email, password=None):
+
+        if password is None:
+            raise TypeError('Users should have a password')
+      
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # db_index=True (i think) indexes based on new usernames
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.CharField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    #set login to use email instead of username
+    USERNAME_FIELD = 'email'
+    # custom req field below, can add more if desired
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        return ''
+
+
 
 # class Ingredient(models.Model):  # Ingredient to be used in a recipe
 #     name = models.CharField(max_length=255)
