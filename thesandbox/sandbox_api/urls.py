dd82b361c1fff8fd56a34e44dc048cfbbb82624c
Alison Reilly <reillyamr@gmail.com>
2020-09-27 12:05:11 -0400

    defined paths based on multiple sandbox examples

--- thesandbox/sandbox_api/urls.py
+++ thesandbox/sandbox_api/urls.py
@@ -19,16 +19,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', PuppyListView.as_view()),
-    path('create/', PuppyCreateView.as_view()),
-    path('<pk>', PuppyDetailView.as_view()),
-    path('<pk>/update/', PuppyUpdateView.as_view()),
-    path('<pk>/delete/', PuppyDeleteView.as_view()),
-    # path('', BucketlistListView.as_view()),
-    # path('create/', BucketlistCreateView.as_view()),
-    # path('<pk>', BucketlistDetailView.as_view()),
-    # path('<pk>/update/', BucketlistUpdateView.as_view()),
-    # path('<pk>/delete/', BucketlistDeleteView.as_view()),
+    path('puppy/', PuppyListView.as_view()),
+    path('puppy/create/', PuppyCreateView.as_view()),
+    path('puppy/<pk>', PuppyDetailView.as_view()),
+    path('puppy/<pk>/update/', PuppyUpdateView.as_view()),
+    path('puppy/<pk>/delete/', PuppyDeleteView.as_view()),
+    # path('bucketlist/', BucketlistListView.as_view()),
+    # path('bucketlist/create/', BucketlistCreateView.as_view()),
+    # path('bucketlist/<pk>', BucketlistDetailView.as_view()),
+    # path('bucketlist/<pk>/update/', BucketlistUpdateView.as_view()),
+    # path('bucketlist/<pk>/delete/', BucketlistDeleteView.as_view()),
     #from custom user tutorial
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- thesandbox/thesandbox/urls.py
+++ thesandbox/thesandbox/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-# from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
 from sandbox_api import views
 
-
-router = routers.DefaultRouter()
+# this was from docs intro to using default User and Group but can be done differently anyway, and currently not in use
+# router = routers.DefaultRouter()
 # from docs intro to default User and Groups models
 # router.register(r'users', views.UserViewSet)
 # router.register(r'groups', views.GroupViewSet)
@@ -13,11 +12,11 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('sandbox_api.urls')),
-    # from custom user tutorial
-    path('auth/', include('sandbox_api.urls'))
+    # from custom user tutorial, but sandbox_api.urls already included above
+    # if needed to name the register/ path with pre-fix of auth should prefix in app's urls
+    # path('auth/', include('sandbox_api.urls'))
 
 
 ]
