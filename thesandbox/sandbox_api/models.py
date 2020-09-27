9f446edc2d02552b8049a6d20fb161513a3173a7
Alison Reilly <reillyamr@gmail.com>
2020-09-27 13:52:57 -0400

    try different approach to custom user in models

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -1,49 +1,52 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+    def create_user(self, email, username, password=None):
 
-        if username is None:
-            raise TypeError('Users should have a username')
-        if email is None:
-            raise TypeError('Users should have an email')
-
-        user = self.model(username=username, email=self.normalize_email(email))    
+        if not username:
+            raise ValueError("Unique username is required")
+        if not email:
+            raise ValueError("User must have an email address")
+        if not password:
+            raise ValueError("Password is required")
+        user = self.model(
+            email=self.normalize_username(username)
+        )
         user.set_password(password)
-        user.save()
+        user.username = username
+        user.email = email
+        user.save(using=self._db)
+        return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, email, username, password):
 
-        if password is None:
-            raise TypeError('Users should have a password')
-      
-        user = self.create_user(username, email, password)
+        user = self.create_user(username = username, email = email,  password=password)
         user.is_superuser = True
         user.is_staff = True
-        user.save()
+        user.is_admin = True
+        user.save(using=self._db)
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser):
     # db_index=True (i think) indexes based on new usernames
-    username = models.CharField(max_length=255, unique=True, db_index=True)
-    email = models.CharField(max_length=255, unique=True, db_index=True)
+    username = models.CharField(unique=True, max_length=255)
+    email = models.EmailField(unique=True, max_length=255)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-    #set login to use email instead of username
-    USERNAME_FIELD = 'email'
-    # custom req field below, can add more if desired
-    REQUIRED_FIELDS = ['username']
-
     objects = UserManager()
 
+    USERNAME_FIELD = 'username'  # username
+    REQUIRED_FIELDS = []  #['full_name']
+    
     def __str__(self):
         return self.email
 
