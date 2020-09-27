488e6bcaf06e66a429ec53549a28e7682546bdb3
Alison Reilly <reillyamr@gmail.com>
2020-09-27 14:06:48 -0400

    redo database and follow email only example

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -2,56 +2,42 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
 class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-
-        if not username:
-            raise ValueError("Unique username is required")
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
         if not email:
-            raise ValueError("User must have an email address")
-        if not password:
-            raise ValueError("Password is required")
+            raise ValueError('Users must have an email address')
         user = self.model(
-            email=self.normalize_username(username)
+            email=self.normalize_email(email),
         )
         user.set_password(password)
-        user.username = username
-        user.email = email
         user.save(using=self._db)
         return user
-
-    def create_superuser(self, email, username, password):
-
-        user = self.create_user(username = username, email = email,  password=password)
-        user.is_superuser = True
-        user.is_staff = True
+    
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and
+        password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
         user.is_admin = True
         user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser):
-    # db_index=True (i think) indexes based on new usernames
-    username = models.CharField(unique=True, max_length=255)
-    email = models.EmailField(unique=True, max_length=255)
-    is_verified = models.BooleanField(default=False)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
     objects = UserManager()
-
-    USERNAME_FIELD = 'username'  # username
-    REQUIRED_FIELDS = []  #['full_name']
-    
+    USERNAME_FIELD = 'email'
     def __str__(self):
         return self.email
-
-    def tokens(self):
-        return ''
 
 
 
