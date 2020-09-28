58c85fada0a6f03fe509daed36b96935f57af688
Alison Reilly <reillyamr@gmail.com>
2020-09-28 19:23:44 -0400

    add is_verified to coordinate w activation token

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -34,6 +34,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    is_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
