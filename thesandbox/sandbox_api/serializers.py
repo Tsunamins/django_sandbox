b70f136e7a3002319349e8fb3281389b3dbb3f67
Alison Reilly <reillyamr@gmail.com>
2020-10-02 15:44:27 -0400

    add tokens, fields & responses based on auth error

--- thesandbox/sandbox_api/serializers.py
+++ thesandbox/sandbox_api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 # from custom User model
 from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=68, write_only=True)
@@ -19,13 +21,36 @@ class EmailVerificationSerilaizer(serializers.ModelSerializer):
 
 class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
-    password = serializers.CharField(max_length=68, min_length=6)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    # username = serializers.CharField(max_length=255, min_length=3, read_only=True)
+    tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'tokens'] # tutorial has username also, but currently no username in this example
 
     def validate(self, attrs):
         email = attrs.get('email', '')
         password = attrs.get('password', '')
+        user = auth.authenticate(email=email, password=password)
+        if not user:
+            raise AuthenticationFailed('Invalid Credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+     
 
-        
+        return {
+            'email': user.email,
+            # 'username': user.username,
+            #tutorial says should call tokens() method
+            'tokens': user.tokens
+        }
+
+        # return super().validatea(attrs)
+
+
 
 
         
