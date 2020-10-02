d9442069b9de7728a0a55f1689298f69185c6fb2
Alison Reilly <reillyamr@gmail.com>
2020-10-02 15:11:24 -0400

    add email verification and login serializers

--- thesandbox/sandbox_api/serializers.py
+++ thesandbox/sandbox_api/serializers.py
@@ -10,6 +10,24 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'password']
 
+class EmailVerificationSerilaizer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6)
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+
+        
+
+
         
 
 
