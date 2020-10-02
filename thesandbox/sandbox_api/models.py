5e4f81cd7829c6f60f0d2f3022486a705116ab47
Alison Reilly <reillyamr@gmail.com>
2020-10-02 15:31:19 -0400

    add tokens def to User

--- thesandbox/sandbox_api/models.py
+++ thesandbox/sandbox_api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
@@ -43,6 +44,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     def __str__(self):
         return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+       
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
 
 
 
